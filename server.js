71fe84b1eb19cc5fda560424b54273a1b39eec99
iseng112358 <ivans112358@gmail.com>
2017-04-24 11:02:41 -0400

    added to server

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
-var express = require('express');
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost/scores');
+
+var ScoreSchema = mongoose.Schema({
+  "name": String,
+  "experience": Number,
+  "date": String
+});
+
+var Score = mongoose.model("Score", ScoreSchema);
+
+app.post("/addScore", function(req, res) {
+  var newScore = new Score(req.body);
+  newScore.save(function(err,res) {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else res.send("Updated");
+  });
+});
+
+app.get("/getScores", function(req, res) {
+  Score.find(req.query, function(err, score) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(score);
+      res.json(score);
+    }
+  }
+)
+});
 
 app.listen(3000, function(){
   console.log('Listening on Port 3000');
